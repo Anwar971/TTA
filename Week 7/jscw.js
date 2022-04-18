fdb129527fb8552cd3c9f70d59708a6c2f0cea50
Anwar971 <101737262+Anwar971@users.noreply.github.com>
2022-04-18 13:08:54 +0100

    Add files via upload

---
+++ Week 7/jscw.js
@@ -0,0 +1,7 @@
+function light_mode_button() {// button function change backround tint - dont work not sure why//
+    var light_mode = document.body;
+    element.classList.toggle("light_mode_button");
+ }
+
+ 
+
